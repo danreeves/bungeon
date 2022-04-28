b6625087d216a81efd701b86258250bcbf8a0464
Dan Reeves <hey@danreev.es>
2022-04-29 00:39:17 +0100

    refactor

--- main.lua
+++ main.lua
@@ -1,6 +1,24 @@
 local inspect = require("inspect")
+local class = require("oops")
 local curses = require("curses")
 local stdscr = curses.initscr()
+
+local function contains(list, x)
+	for _, v in pairs(list) do
+		if v == x then
+			return true
+		end
+	end
+	return false
+end
+
+local function map(list, fn)
+	local l = {}
+	for _, v in ipairs(list) do
+		table.insert(l, fn(v))
+	end
+	return l
+end
 
 local room_size_goal = {
 	width = 20,
@@ -17,66 +35,71 @@ local game = {
 
 math.randomseed(tonumber(game.seed))
 
--- TreeNode
--- x: 0
--- y: 0
--- height: number
--- width: number
--- children: { TreeNode, TreeNode }
-local bsp_tree = {
-	x = 0,
-	y = 0,
-	height = game.height - 0,
-	width = game.width - 0,
-	children = {},
-}
+local tn_count = 0
+local TreeNode = class("TreeNode")({
+	__init = function(self, x, y, height, width, parent)
+		self.x = x or 0
+		self.y = y or 0
+		self.height = height or 0
+		self.width = width or 0
+		self.children = {}
+		self.room = nil
+		self.parent = parent
+		tn_count = tn_count + 1
+		self._id = tn_count
+	end,
+
+	to_string = function(self)
+		return "TreeNode:" .. tostring(self._id)
+	end,
+
+	add_child = function(self, child)
+		table.insert(self.children, child)
+	end,
+
+	is_wall = function(self, x, y)
+		local isWallX = (x >= self.x and x <= self.x + self.width - 1)
+			and (y == self.y or y == self.y + self.height - 1)
+		local isWallY = (y >= self.y and y <= self.y + self.height - 1)
+			and (x == self.x or x == self.x + self.width - 1)
+		return isWallX or isWallY
+	end,
+
+	is_inside = function(self, x, y)
+		return (x > self.x and x < self.x + self.width - 1) and (y > self.y and y < self.y + self.height - 1)
+	end,
+
+	get_siblings = function(self)
+		if self.parent == nil then
+			return {}
+		end
+		-- siblings including self
+		return self.parent.children
+	end,
+
+	get_sibling_rooms = function(self)
+		local siblings = self:get_siblings()
+		local rooms = {}
+		for _, sibling in ipairs(siblings) do
+			table.insert(rooms, sibling.room)
+		end
+		return rooms
+	end,
+})
+
+local bsp_tree = TreeNode(0, 0, game.height - 0, game.width - 0)
 
 local function make_partition(node, verticalOverride)
 	local vertical = verticalOverride ~= nil and verticalOverride or math.random() > 0.5
 	local val = vertical and node.width or node.height
 	local margin = val / 100 * 10 -- 10% of the height or width
 	local split = math.floor(math.random((val / 2) - margin, (val / 2) + margin)) -- split roughly halfway, plus or minus the 10% margin
-
 	local gap = game.room_size_goal.padding
-	local child1 = vertical
-			and {
-				x = node.x,
-				y = node.y,
-				width = split - gap,
-				height = node.height,
-				children = {},
-				rooms = {},
-				parent = node,
-			}
-		or {
-			x = node.x,
-			y = node.y,
-			width = node.width,
-			height = split - gap,
-			children = {},
-			rooms = {},
-			parent = node,
-		}
 
-	local child2 = vertical
-			and {
-				x = node.x + split + gap,
-				y = node.y,
-				width = node.width - (split + gap),
-				height = node.height,
-				children = {},
-				rooms = {},
-				parent = node,
-			}
-		or {
-			x = node.x,
-			y = node.y + split + gap,
-			width = node.width,
-			height = node.height - (split + gap),
-			children = {},
-			rooms = {},
-			parent = node,
-		}
+	local child1 = vertical and TreeNode(node.x, node.y, node.height, split - gap, node)
+		or TreeNode(node.x, node.y, split - gap, node.width, node)
+	local child2 = vertical and TreeNode(node.x + split + gap, node.y, node.height, node.width - (split + gap), node)
+		or TreeNode(node.x, node.y + split + gap, node.height - (split + gap), node.width, node)
 
 	if
 		child1.width >= game.room_size_goal.width
@@ -85,10 +108,10 @@ local function make_partition(node, verticalOverride)
 		and child2.height >= game.room_size_goal.height
 	then
 		make_partition(child1)
-		table.insert(node.children, child1)
+		node:add_child(child1)
 
 		make_partition(child2)
-		table.insert(node.children, child2)
+		node:add_child(child2)
 	elseif verticalOverride == nil then
 		-- If the halfs are too small, try splitting in the other orientation
 		make_partition(node, not vertical)
@@ -118,14 +141,29 @@ for _, leaf in ipairs(leafs) do
 	local width = math.random(game.room_size_goal.min_size, leaf.width)
 	local x = math.random(leaf.x, leaf.x + (leaf.width - width))
 	local y = math.random(leaf.y, leaf.y + (leaf.height - height))
-	local room = {
-		x = x,
-		y = y,
-		height = height,
-		width = width,
-	}
-	table.insert(leaf.rooms, room)
+	local room = TreeNode(x, y, height, width)
+	leaf.room = room
 	table.insert(rooms, room)
+end
+
+local done = {}
+
+for _, leaf in ipairs(leafs) do
+	local parent = leaf.parent
+	if not contains(done, parent) then
+		local rooms_in_parent = leaf:get_sibling_rooms()
+		print(
+			"joining",
+			table.concat(
+				map(rooms_in_parent, function(r)
+					return r:to_string()
+				end),
+				", "
+			),
+			"\r"
+		)
+		table.insert(done, parent)
+	end
 end
 
 local function draw()
@@ -138,14 +176,8 @@ local function draw()
 		for x = 0, game.width do
 			for y = 0, game.height do
 				for _, room in ipairs(nodes) do
-					local isWallX = (x >= room.x and x <= room.x + room.width - 1)
-						and (y == room.y or y == room.y + room.height - 1)
-					local isWallY = (y >= room.y and y <= room.y + room.height - 1)
-						and (x == room.x or x == room.x + room.width - 1)
-					local isFloor = (x > room.x and x < room.x + room.width - 1)
-						and (y > room.y and y < room.y + room.height - 1)
-					local char = (isWallX or isWallY) and "#" or ""
-					if isFloor then
+					local char = room:is_wall(x, y) and "#" or ""
+					if room:is_inside(x, y) then
 						char = string.char(_ + 96) --"."
 					end
 					if char ~= "" then
@@ -174,4 +206,4 @@ local function draw()
 	curses.endwin()
 end
 
-draw()
+-- draw()
